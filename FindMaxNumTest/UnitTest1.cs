cca777a2ab406286e9c61c08c518e1a4ea825f63
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-02-21 10:24:29 +0530

    Max Number at 2nd Position return the Same Number

--- FindMaxNumTest/UnitTest1.cs
+++ FindMaxNumTest/UnitTest1.cs
@@ -3,12 +3,19 @@ namespace FindMaxNumTest
 {
     public class FindMaxTestsForNormalIntClass
     {
-        FindMax findMaxInt = new FindMax();
+        FindMax findMax = new FindMax();
 
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
-            int result = findMaxInt.IntNumber(26, 17, 1);
+            int result = findMax.IntNumber(26, 17, 1);
+            Assert.AreEqual(26, result);
+        }
+
+        [Test]
+        public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            int result = findMax.IntNumber(17, 26, 1);
             Assert.AreEqual(26, result);
         }
     }

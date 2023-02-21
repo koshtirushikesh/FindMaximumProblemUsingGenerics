a0ec95d669d0e7e243f395412f47dc273c2877dd
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-02-21 12:14:14 +0530

    Max String at 2st Position return the Same String

--- FindMaxNumTest/UnitTest1.cs
+++ FindMaxNumTest/UnitTest1.cs
@@ -54,8 +54,14 @@ namespace FindMaxNumTest
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFirstMax()
         {
-            float result = findMax.FloatNumber(26.6f, 17.7f, 1.1f);
-            Assert.AreEqual(26.6f, result);
+            string result = findMax.StringValue("Peach", "Apple", "Banana");
+            Assert.AreEqual("Peach", result);
+        }
+        [Test]
+        public void GivenMaxSecondNum_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            string result = findMax.StringValue("Apple", "Peach", "Banana");
+            Assert.AreEqual("Peach", result);
         }
 
     }

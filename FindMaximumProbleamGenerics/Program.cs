43d3eb9940c51f2373ae17cfa37de1cb15e06bc8
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-02-21 10:56:18 +0530

    Given 3 Floats find the maximum

--- FindMaximumProbleamGenerics/Program.cs
+++ FindMaximumProbleamGenerics/Program.cs
@@ -13,7 +13,7 @@ namespace FindMaximumProbleamGenerics
         {
             FindMax findMax = new FindMax();
             Console.WriteLine(findMax.IntNumber(50, 40, 30));
-
+            Console.WriteLine(findMax.FloatNumber(20.2f,33.3f,44.4f));
             Console.ReadLine();
         }
     }
